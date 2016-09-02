688ac38b2a5fe52d358a5dc332584c4cf628f5a8
Jack Wu <jackchased@gmail.com>
2016-09-02 10:19:37 +0800

    view components of Humidity, Temperature, and Illuminance

--- app/components/Card/Humidity.js
+++ app/components/Card/Humidity.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import HumidityIcon from '../../static/humid.png';
 
-var HumidityCard = React.createClass({
-    render: function () {
-        return (
-            <div style={{width: '100%', height: '100%', backgroundColor: '#30FFFF'}}>
-                HumidityCard
+var bgColor = "#c2c2d6";
+
+const Humidity = ({humid}) => {
+    return (
+        <div style={{width: "100%", height: "100%", backgroundColor: bgColor }}>
+            <div style={{float: "left", width: "50%", height: "100%"}}>
+                <div style={{position: "relative", top: "50%", left: "25%" , transform: "translateY(-50%)"}}>
+                    <img src={HumidityIcon} style={{width: "50%", height: "50%"}} />
+                </div>
             </div>
-        );
-    }
-});
 
+            <div style={{float: "left", width: "50%", height: "100%"}}>
+                <div style={{position: "absolute", top: "0", bottom: "0", left: "50%", right: "0", margin: "0", textAlign: "center", fontSize: "36px", lineHeight: "130px"}}>
+                    60 %
+                </div>
+            </div>
+        </div>
+    );
+}
 
-module.exports = HumidityCard;
+export default Humidity
--- app/components/Card/Illuminance.js
+++ app/components/Card/Illuminance.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
-var IlluminanceCard = React.createClass({
-    render: function () {
-        return (
-            <div style={{width: '100%', height: '100%', backgroundColor: '#30FFFF'}}>
-                IlluminanceCard
+var bgColor = "#c2c2d6";
+
+const Illuminance = ({lux}) => {
+    return (
+        <div style={{width: "100%", height: "100%", backgroundColor: bgColor }}>
+            <div style={{float: "left", width: "50%", height: "100%"}}>
+                <div style={{position: "relative", top: "15%", left: "15%", width: "70%", height: "70%"}}>
+                    <svg fill="white" height="100%" viewBox="0 0 24 24" width="100%" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.79 1.42-1.41zM4 10.5H1v2h3v-2zm9-9.95h-2V3.5h2V.55zm7.45 3.91l-1.41-1.41-1.79 1.79 1.41 1.41 1.79-1.79zm-3.21 13.7l1.79 1.8 1.41-1.41-1.8-1.79-1.4 1.4zM20 10.5v2h3v-2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm-1 16.95h2V19.5h-2v2.95zm-7.45-3.91l1.41 1.41 1.79-1.8-1.41-1.41-1.79 1.8z"/>
+                    </svg>
+                </div>
             </div>
-        );
-    }
-});
 
+            <div style={{float: "left", width: "50%", height: "100%"}}>
+                <div style={{position: "absolute", top: "0", bottom: "0", left: "50%", right: "0", margin: "0", textAlign: "center", fontSize: "34px", lineHeight: "130px"}}>
+                    205 lx
+                </div>
+            </div>
+        </div>
+    );
+}
 
-module.exports = IlluminanceCard;
+export default Illuminance
